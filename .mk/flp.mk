338dd6e5f62ba0c6f3bf9a6e3b7b2a9ca30cf16b
Kalman Meth <meth@il.ibm.com>
2022-06-08 12:11:36 +0300

    changed name of configuration file

--- .mk/flp.mk
+++ .mk/flp.mk
@@ -4,9 +4,9 @@
 generate-flp-configuration: ## Generate metrics configuration
 	@echo -e "\n==> Generate metrics configuration"
 	cp -r network_definitions /tmp/network_definitions
-	$(OCI_BIN) run --entrypoint /app/confgenerator -v /tmp/network_definitions:/app/network_definitions -v /tmp/destDir:/app/destDir quay.io/netobserv/flowlogs-pipeline:latest --srcFolder /app/network_definitions --destConfFile /app/destDir/flowlogs-configuration.yaml
+	$(OCI_BIN) run --entrypoint /app/confgenerator -v /tmp/network_definitions:/app/network_definitions -v /tmp/destDir:/app/destDir quay.io/netobserv/flowlogs-pipeline:latest --srcFolder /app/network_definitions --destConfFile /app/destDir/flowlogs-pipeline.conf.yaml
 	# TODO: add --destDocFile and --destGrafanaJsonnetFolder
-	cp /tmp/destDir/flowlogs-configuration.yaml config/flp/flowlogs-configuration.yaml
+	cp /tmp/destDir/flowlogs-pipeline.conf.yaml config/flp/flowlogs-pipeline.conf.yaml
 	@echo -e "\n===> flowlogs-configuration.yaml available in config/flp/flowlogs-configuration.yaml \n"
 	# TODO: delete container
 
