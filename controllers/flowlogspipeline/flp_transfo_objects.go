057556f30166871ff5058885955d894832c4fefb
Joel Takvorian <jtakvori@redhat.com>
2022-09-21 09:58:29 +0200

    FLP reconciler refactoring
    Decoupling the inner "single reconcilers" into different files and
    structs:
      - Main entry point is still flp_reconciler.go, which now only
        initialises and calls inner reconcilers
      - ingest/transfo/monolith inner reconcilers do their own stuff in a
        decoupled way, with their own "*_objects" files
      - "flp_common_objects" is like a common lib for the specialized
        objects files

---
+++ controllers/flowlogspipeline/flp_transfo_objects.go
@@ -0,0 +1,135 @@
+package flowlogspipeline
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	ascv2 "k8s.io/api/autoscaling/v2beta2"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/netobserv/flowlogs-pipeline/pkg/api"
+	"github.com/netobserv/flowlogs-pipeline/pkg/config"
+	flowsv1alpha1 "github.com/netobserv/network-observability-operator/api/v1alpha1"
+)
+
+type transfoBuilder struct {
+	generic builder
+}
+
+func newTransfoBuilder(ns string, desired *flowsv1alpha1.FlowCollectorSpec, useOpenShiftSCC bool) transfoBuilder {
+	gen := newBuilder(ns, desired, ConfKafkaTransformer, useOpenShiftSCC)
+	return transfoBuilder{
+		generic: gen,
+	}
+}
+
+func (b *transfoBuilder) deployment(configDigest string) *appsv1.Deployment {
+	pod := b.generic.podTemplate(false /*no listen*/, true /*loki itf*/, false /*no host network*/, configDigest)
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      b.generic.name(),
+			Namespace: b.generic.namespace,
+			Labels:    b.generic.labels,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &b.generic.desired.Processor.Replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: b.generic.selector,
+			},
+			Template: pod,
+		},
+	}
+}
+
+func (b *transfoBuilder) configMap() (*corev1.ConfigMap, string, error) {
+	stages, params, err := b.buildPipelineConfig()
+	if err != nil {
+		return nil, "", err
+	}
+	return b.generic.configMap(stages, params)
+}
+
+func (b *transfoBuilder) buildPipelineConfig() ([]config.Stage, []config.StageParam, error) {
+	// TODO in a later optimization patch: set ingester <-> transformer communication also via protobuf
+	// For now, we leave this communication via JSON and just setup protobuf ingestion when
+	// the transformer is communicating directly via eBPF agent
+	decoder := api.Decoder{Type: "protobuf"}
+	if b.agent == flowsv1alpha1.AgentIPFIX {
+		decoder = api.Decoder{Type: "json"}
+	}
+	pipeline := config.NewKafkaPipeline("kafka-read", api.IngestKafka{
+		Brokers: []string{b.generic.desired.Kafka.Address},
+		Topic:   b.generic.desired.Kafka.Topic,
+		GroupId: b.generic.name(), // Without groupid, each message is delivered to each consumers
+		Decoder: decoder,
+		TLS:     b.generic.getKafkaTLS(),
+	})
+
+	err := b.generic.addTransformStages(&pipeline)
+	if err != nil {
+		return nil, nil, err
+	}
+	return pipeline.GetStages(), pipeline.GetStageParams(), nil
+}
+
+func (b *transfoBuilder) newPromService() *corev1.Service {
+	return b.generic.newPromService()
+}
+
+func (b *transfoBuilder) fromPromService(old *corev1.Service) *corev1.Service {
+	return b.generic.fromPromService(old)
+}
+
+func (b *transfoBuilder) autoScaler() *ascv2.HorizontalPodAutoscaler {
+	return &ascv2.HorizontalPodAutoscaler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      b.generic.name(),
+			Namespace: b.generic.namespace,
+			Labels:    b.generic.labels,
+		},
+		Spec: ascv2.HorizontalPodAutoscalerSpec{
+			ScaleTargetRef: ascv2.CrossVersionObjectReference{
+				APIVersion: "apps/v1",
+				Kind:       "Deployment",
+				Name:       b.generic.name(),
+			},
+			MinReplicas: b.generic.desired.Processor.HPA.MinReplicas,
+			MaxReplicas: b.generic.desired.Processor.HPA.MaxReplicas,
+			Metrics:     b.generic.desired.Processor.HPA.Metrics,
+		},
+	}
+}
+
+// The operator needs to have at least the same permissions as flowlogs-pipeline in order to grant them
+//+kubebuilder:rbac:groups=apps,resources=replicasets,verbs=get;list;watch
+//+kubebuilder:rbac:groups=autoscaling,resources=horizontalpodautoscalers,verbs=create;delete;patch;update;get;watch;list
+//+kubebuilder:rbac:groups=core,resources=pods;services;nodes,verbs=get;list;watch
+
+func buildClusterRoleTransformer() *rbacv1.ClusterRole {
+	return &rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name(ConfKafkaTransformer),
+		},
+		Rules: []rbacv1.PolicyRule{{
+			APIGroups: []string{""},
+			Verbs:     []string{"list", "get", "watch"},
+			Resources: []string{"pods", "services", "nodes"},
+		}, {
+			APIGroups: []string{"apps"},
+			Verbs:     []string{"list", "get", "watch"},
+			Resources: []string{"replicasets"},
+		}, {
+			APIGroups: []string{"autoscaling"},
+			Verbs:     []string{"create", "delete", "patch", "update", "get", "watch", "list"},
+			Resources: []string{"horizontalpodautoscalers"},
+		}},
+	}
+}
+
+func (b *transfoBuilder) serviceAccount() *corev1.ServiceAccount {
+	return b.generic.serviceAccount()
+}
+
+func (b *transfoBuilder) clusterRoleBinding() *rbacv1.ClusterRoleBinding {
+	return b.generic.clusterRoleBinding(ConfKafkaTransformer)
+}
