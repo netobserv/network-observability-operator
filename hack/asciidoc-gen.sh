4ecec2d17fe0fbb46722e42a6edbdb281e812055
Joel Takvorian <jtakvori@redhat.com>
2022-12-21 18:26:44 +0100

    Automate asciidoc generation

---
+++ hack/asciidoc-gen.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p _tmp
+oc get --raw /openapi/v2 | jq . > _tmp/openapi.json
+
+jq '.definitions |= ({"io.netobserv.flows.v1alpha1.FlowCollector"})
+  | del(.definitions."io.netobserv.flows.v1alpha1.FlowCollector".properties.metadata."$ref")
+  | .definitions."io.netobserv.flows.v1alpha1.FlowCollector".properties.metadata += {type:"object"}
+  | del(.definitions."io.netobserv.flows.v1alpha1.FlowCollector".properties.spec.properties.consolePlugin.properties.autoscaler.properties)
+  | del(.definitions."io.netobserv.flows.v1alpha1.FlowCollector".properties.spec.properties.processor.properties.kafkaConsumerAutoscaler.properties)
+  | .definitions."io.netobserv.flows.v1alpha1.FlowCollector".properties.spec.properties.consolePlugin.properties.autoscaler.description |= . + " Please refer to HorizontalPodAutoscaler documentation (autoscaling/v2)."
+  | .definitions."io.netobserv.flows.v1alpha1.FlowCollector".properties.spec.properties.processor.properties.kafkaConsumerAutoscaler.description |= . + " Please refer to HorizontalPodAutoscaler documentation (autoscaling/v2)."' \
+  _tmp/openapi.json > _tmp/openapi-amended.json
+
+openshift-apidocs-gen build -c hack/asciidoc-gen-config.yaml _tmp/openapi-amended.json
+
+ADOC=docs/flowcollector-flows-netobserv-io-v1alpha1.adoc
+
+mv docs/flows_netobserv_io/flowcollector-flows-netobserv-io-v1alpha1.adoc $ADOC
+rm docs/index.adoc
+rm -rf docs/objects
+rm -rf docs/flows_netobserv_io
+
+sed -i -r 's/^:_content-type: ASSEMBLY$/:_content-type: REFERENCE/' $ADOC
+sed -i -r 's/^\[id="flowcollector-flows-netobserv-io-v.+"\]$/[id="network-observability-flowcollector-api-specifications_{context}"]/' $ADOC
+sed -i -r 's/= FlowCollector \[flows.netobserv.io.*/= FlowCollector API specifications/' $ADOC
+sed -i -r '/^:toc: macro$/d ' $ADOC
+sed -i -r '/^:toc-title:$/d ' $ADOC
+sed -i -r '/^toc::\[\]$/d ' $ADOC
+sed -i -r '/^== Specification$/d ' $ADOC
+sed -i -r 's/^==/=/g' $ADOC
+
+sed -i -r '/^= API endpoints/Q' $ADOC
