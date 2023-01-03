b86cdef1cdc63144c96a4474e3601b5456758b62
Kalman Meth <meth@il.ibm.com>
2023-01-03 09:58:31 +0200

    removed cleanup

--- hack/enable-metrics.sh
+++ hack/enable-metrics.sh
@@ -14,14 +14,8 @@ data:
 EOF
 }
 
-cleanup() {
-  echo "--> Cleaning up old deployment"
-  kubectl delete --ignore-not-found=true configMap cluster-monitoring-config
-}
-
 main() {
   echo -e "\n====> Enabling flp metrics in console \n"
-  cleanup
   enable_flp_metrics
   echo -e "\n====> Done."
 }
