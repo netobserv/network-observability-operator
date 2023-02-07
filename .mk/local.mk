8fd752143261f5bb6e3b65b46c07a9ce9d07bb20
msherif1234 <mmahmoud@redhat.com>
2023-02-07 16:14:43 -0500

    refactor config to allow deployment on OCP or KinD platforms
    Signed-off-by: msherif1234 <mmahmoud@redhat.com>

--- .mk/local.mk
+++ .mk/local.mk
@@ -1,4 +1,16 @@
 ##@ Local (Kind)
+CERT_MANAGER_VERSION=v1.9.1
+CERT_MANAGER_URL ?= "https://github.com/cert-manager/cert-manager/releases/download/$(CERT_MANAGER_VERSION)/cert-manager.yaml"
+
+.PHONY: install-cert-manager
+install-cert-manager: ## Install cert manager onto the target kubernetes cluster
+	set -e ;\
+	kubectl apply -f $(CERT_MANAGER_URL) ;\
+	hack/wait_for_cert_manager.sh ;\
+
+.PHONY: uninstall-cert-manager
+uninstall-cert-manager: ## Uninstall cert manager from the target kubernetes cluster
+	kubectl delete -f $(CERT_MANAGER_URL)
 
 .PHONY: create-kind-cluster
 create-kind-cluster: $(KIND) ## Create kind cluster
@@ -10,7 +22,7 @@ delete-kind-cluster: $(KIND) ## Delete kind cluster
 	$(KIND) delete cluster
 
 .PHONY: local-deploy
-local-deploy: create-kind-cluster deploy-all  ## Local deploy (kind, loki, grafana, example-cr and sample-workload excluding the operator)
+local-deploy: create-kind-cluster install-cert-manager deploy-all  ## Local deploy (kind, loki, grafana, example-cr and sample-workload excluding the operator)
 
 .PHONY: clean-leftovers
 clean-leftovers:
@@ -21,7 +33,7 @@ clean-leftovers:
 local-redeploy: clean-leftovers undeploy-all deploy-all  ## Local re-deploy (loki, grafana, example-cr and sample-workload excluding the operator)
 
 .PHONY: local-undeploy
-local-undeploy: clean-leftovers undeploy-all delete-kind-cluster  ## Local cleanup
+local-undeploy: clean-leftovers uninstall-cert-manager undeploy-all delete-kind-cluster  ## Local cleanup
 
 local-run: create-kind-cluster local-redeploy local-deploy-operator ## local-redeploy + run the operator locally
 
@@ -39,3 +51,15 @@ local-deploy-operator:
 	@echo "====> Operator process info"
 	@PID=$$(pgrep --oldest --full "main.go"); echo -e "\n===> The operator is running in process $$PID\nTo stop the operator process use: pkill -p $$PID"
 	@echo "====> Done"
+
+.PHONY: deploy-kind
+deploy-kind: generate kustomize ## Deploy controller to the K8s cluster specified in ~/.kube/config.
+	cd config/manager && $(KUSTOMIZE) edit set image controller=${IMG}
+	$(SED) -i -r 's~ebpf-agent:.+~ebpf-agent:main~' ./config/manager/manager.yaml
+	$(SED) -i -r 's~flowlogs-pipeline:.+~flowlogs-pipeline:main~' ./config/manager/manager.yaml
+	$(SED) -i -r 's~console-plugin:.+~console-plugin:main~' ./config/manager/manager.yaml
+	$(KUSTOMIZE) build config/kubernetes | kubectl apply -f -
+
+.PHONY: undeploy-kind
+undeploy-kind: ## Undeploy controller from the K8s cluster specified in ~/.kube/config.
+	$(KUSTOMIZE) build config/kubernetes | kubectl --ignore-not-found=true delete -f - || true
