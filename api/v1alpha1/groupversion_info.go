000bebdc368bff43a7a69e8a75bd9b43db333fa4
Joel Takvorian <jtakvori@redhat.com>
2023-02-13 17:34:02 +0100

    Move to go1.19

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the flows v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=flows.netobserv.io
+// +kubebuilder:object:generate=true
+// +groupName=flows.netobserv.io
 package v1alpha1
 
 import (
