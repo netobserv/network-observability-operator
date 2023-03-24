c34f71e5297b4db644fd089556468f5a2740a311
msherif1234 <mmahmoud@redhat.com>
2023-03-24 11:22:44 -0400

    NETOBSERV-904: Make v1beta1 the stored version
    Signed-off-by: msherif1234 <mmahmoud@redhat.com>

--- api/v1beta1/flowcollector_types.go
+++ api/v1beta1/flowcollector_types.go
@@ -705,6 +705,8 @@ type FlowCollectorStatus struct {
 // +kubebuilder:printcolumn:name="Sampling (EBPF)",type="string",JSONPath=`.spec.agent.ebpf.sampling`
 // +kubebuilder:printcolumn:name="Deployment Model",type="string",JSONPath=`.spec.deploymentModel`
 // +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[*].reason"
+// +kubebuilder:storageversion
+
 // FlowCollector is the Schema for the flowcollectors API, which pilots and configures netflow collection.
 type FlowCollector struct {
 	metav1.TypeMeta   `json:",inline"`
