34965b449595d84580e8ee3a591bc67253e0364a
Mohamed S. Mahmoud <mmahmoud@redhat.com>
2024-01-18 10:57:32 -0500

    NETOBSERV-1298: Add netobserv operator changes to support dup list (#501)
    * NETOBSERV-1298: Add netobserv operator changes to support dup list
    
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>
    
    * add columns definitions to new list fields
    
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>
    
    * deduper mode frontend config
    
    ---------
    
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>
    Co-authored-by: Julien Pinsonneau <jpinsonn@redhat.com>

--- controllers/ebpf/agent_controller.go
+++ controllers/ebpf/agent_controller.go
@@ -51,8 +51,6 @@ const (
 	envLogLevel                   = "LOG_LEVEL"
 	envDedupe                     = "DEDUPER"
 	dedupeDefault                 = "firstCome"
-	envDedupeJustMark             = "DEDUPER_JUST_MARK"
-	dedupeJustMarkDefault         = "true"
 	envGoMemLimit                 = "GOMEMLIMIT"
 	envEnablePktDrop              = "ENABLE_PKT_DROPS"
 	envEnableDNSTracking          = "ENABLE_DNS_TRACKING"
@@ -69,6 +67,13 @@ const (
 	bpfTraceMountPath  = "/sys/kernel/debug"
 	bpfNetNSMountName  = "var-run-netns"
 	bpfNetNSMountPath  = "/var/run/netns"
+)
+
+const (
+	EnvDedupeJustMark     = "DEDUPER_JUST_MARK"
+	EnvDedupeMerge        = "DEDUPER_MERGE"
+	DedupeJustMarkDefault = "true"
+	DedupeMergeDefault    = "false"
 )
 
 type reconcileAction int
@@ -460,21 +465,24 @@ func (c *AgentController) setEnvConfig(coll *flowslatest.FlowCollector) []corev1
 	}
 
 	dedup := dedupeDefault
-	dedupJustMark := dedupeJustMarkDefault
+	dedupJustMark := DedupeJustMarkDefault
+	dedupMerge := DedupeMergeDefault
 	// we need to sort env map to keep idempotency,
 	// as equal maps could be iterated in different order
 	for _, pair := range helper.KeySorted(coll.Spec.Agent.EBPF.Debug.Env) {
 		k, v := pair[0], pair[1]
 		if k == envDedupe {
 			dedup = v
-		} else if k == envDedupeJustMark {
+		} else if k == EnvDedupeJustMark {
 			dedupJustMark = v
+		} else if k == EnvDedupeMerge {
+			dedupMerge = v
 		} else {
 			config = append(config, corev1.EnvVar{Name: k, Value: v})
 		}
 	}
 	config = append(config, corev1.EnvVar{Name: envDedupe, Value: dedup})
-	config = append(config, corev1.EnvVar{Name: envDedupeJustMark, Value: dedupJustMark})
+	config = append(config, corev1.EnvVar{Name: EnvDedupeJustMark, Value: dedupJustMark})
 	config = append(config, corev1.EnvVar{
 		Name: envAgentIP,
 		ValueFrom: &corev1.EnvVarSource{
@@ -485,6 +493,7 @@ func (c *AgentController) setEnvConfig(coll *flowslatest.FlowCollector) []corev1
 		},
 	},
 	)
+	config = append(config, corev1.EnvVar{Name: EnvDedupeMerge, Value: dedupMerge})
 
 	return config
 }
