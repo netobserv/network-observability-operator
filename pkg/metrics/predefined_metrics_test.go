ae3eb0617c0dc7e0967aa83f43993bc730143459
Joel Takvorian <jtakvori@redhat.com>
2024-05-30 18:20:13 +0200

    NETOBSERV-1672: add cluster & zone to predefined metrics
    They're only added when those features are enabled.

--- pkg/metrics/predefined_metrics_test.go
+++ pkg/metrics/predefined_metrics_test.go
@@ -43,7 +43,23 @@ func TestIncludeExclude(t *testing.T) {
 func TestGetDefinitions(t *testing.T) {
 	assert := assert.New(t)
 
-	res := GetDefinitions([]string{"namespace_flows_total", "node_ingress_bytes_total", "workload_egress_packets_total"})
+	res := GetDefinitions([]string{"namespace_flows_total", "node_ingress_bytes_total", "workload_egress_packets_total"}, nil)
+	assert.Len(res, 3)
+	assert.Equal("node_ingress_bytes_total", res[0].Spec.MetricName)
+	assert.Equal("Bytes", res[0].Spec.ValueField)
+	assert.Equal([]string{"K8S_Clustername", "SrcK8S_Zone", "DstK8S_Zone", "SrcK8S_HostName", "DstK8S_HostName"}, res[0].Spec.Labels)
+	assert.Equal("namespace_flows_total", res[1].Spec.MetricName)
+	assert.Empty(res[1].Spec.ValueField)
+	assert.Equal([]string{"K8S_Clustername", "SrcK8S_Zone", "DstK8S_Zone", "SrcK8S_Namespace", "DstK8S_Namespace", "K8S_FlowLayer", "SrcSubnetLabel", "DstSubnetLabel"}, res[1].Spec.Labels)
+	assert.Equal("workload_egress_packets_total", res[2].Spec.MetricName)
+	assert.Equal("Packets", res[2].Spec.ValueField)
+	assert.Equal([]string{"K8S_Clustername", "SrcK8S_Zone", "DstK8S_Zone", "SrcK8S_Namespace", "DstK8S_Namespace", "K8S_FlowLayer", "SrcSubnetLabel", "DstSubnetLabel", "SrcK8S_OwnerName", "DstK8S_OwnerName", "SrcK8S_OwnerType", "DstK8S_OwnerType", "SrcK8S_Type", "DstK8S_Type"}, res[2].Spec.Labels)
+}
+
+func TestGetDefinitionsRemoveZoneCluster(t *testing.T) {
+	assert := assert.New(t)
+
+	res := GetDefinitions([]string{"namespace_flows_total", "node_ingress_bytes_total", "workload_egress_packets_total"}, []string{"K8S_Clustername", "SrcK8S_Zone", "DstK8S_Zone"})
 	assert.Len(res, 3)
 	assert.Equal("node_ingress_bytes_total", res[0].Spec.MetricName)
 	assert.Equal("Bytes", res[0].Spec.ValueField)
