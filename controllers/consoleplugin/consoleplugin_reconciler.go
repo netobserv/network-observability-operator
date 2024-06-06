8c0c1eb2c6d844926819a193ce9d77981dc4090c
Joel Takvorian <jtakvori@redhat.com>
2024-06-06 11:32:31 +0200

    NETOBSERV-1647: Do not load loki cert when loki disabled [Backport 1.6] (#670)
    * Do not load loki cert when loki disabled
    
    * NETOBSERV-1665: use v1 for conversionReviewVersions (#662)
    
    Note we're talking here about versions of ConversionReview (ie.
    apiextension), NOT versions of our CRDs
    
    - Use same versions for flowcollector and flowmetrics
    - Use just v1 as the only other (v1beta1) was dropped a while ago (kube
      1.22 / 2021)

--- controllers/consoleplugin/consoleplugin_reconciler.go
+++ controllers/consoleplugin/consoleplugin_reconciler.go
@@ -100,13 +100,15 @@ func (r *CPReconciler) Reconcile(ctx context.Context, desired *flowslatest.FlowC
 			return err
 		}
 
-		// Watch for Loki certificates if necessary; we'll ignore in that case the returned digest, as we don't need to restart pods on cert rotation
-		// because certificate is always reloaded from file
-		if _, err = r.Watcher.ProcessCACert(ctx, r.Client, &r.Loki.TLS, r.Namespace); err != nil {
-			return err
-		}
-		if _, _, err = r.Watcher.ProcessMTLSCerts(ctx, r.Client, &r.Loki.StatusTLS, r.Namespace); err != nil {
-			return err
+		if helper.UseLoki(&desired.Spec) {
+			// Watch for Loki certificates if necessary; we'll ignore in that case the returned digest, as we don't need to restart pods on cert rotation
+			// because certificate is always reloaded from file
+			if _, err = r.Watcher.ProcessCACert(ctx, r.Client, &r.Loki.TLS, r.Namespace); err != nil {
+				return err
+			}
+			if _, _, err = r.Watcher.ProcessMTLSCerts(ctx, r.Client, &r.Loki.StatusTLS, r.Namespace); err != nil {
+				return err
+			}
 		}
 	} else {
 		// delete any existing owned object
