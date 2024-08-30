5219a480ff59aaf670dacebd379ad3230e4feb8b
Mohamed S. Mahmoud <mmahmoud@redhat.com>
2024-08-30 14:34:19 -0400

    NETOBSERV-1777: Allow DNS tracking to use configurable port for tracking (#753)
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>
    (cherry picked from commit 192b5b9ba2fc8df099f4cafeae839661d6904580)

--- controllers/ebpf/agent_controller.go
+++ controllers/ebpf/agent_controller.go
@@ -94,10 +94,12 @@ const (
 )
 
 const (
-	EnvDedupeJustMark     = "DEDUPER_JUST_MARK"
-	EnvDedupeMerge        = "DEDUPER_MERGE"
-	DedupeJustMarkDefault = "false"
-	DedupeMergeDefault    = "true"
+	EnvDedupeJustMark      = "DEDUPER_JUST_MARK"
+	EnvDedupeMerge         = "DEDUPER_MERGE"
+	envDNSTrackingPort     = "DNS_TRACKING_PORT"
+	DedupeJustMarkDefault  = "false"
+	DedupeMergeDefault     = "true"
+	defaultDNSTrackingPort = "53"
 )
 
 // AgentController reconciles the status of the eBPF agent Daemonset, as well as the
@@ -615,6 +617,7 @@ func (c *AgentController) setEnvConfig(coll *flowslatest.FlowCollector) []corev1
 	dedup := dedupeDefault
 	dedupJustMark := DedupeJustMarkDefault
 	dedupMerge := DedupeMergeDefault
+	dnsTrackingPort := defaultDNSTrackingPort
 	// we need to sort env map to keep idempotency,
 	// as equal maps could be iterated in different order
 	advancedConfig := helper.GetAdvancedAgentConfig(coll.Spec.Agent.EBPF.Advanced)
@@ -626,6 +629,8 @@ func (c *AgentController) setEnvConfig(coll *flowslatest.FlowCollector) []corev1
 			dedupJustMark = v
 		} else if k == EnvDedupeMerge {
 			dedupMerge = v
+		} else if k == envDNSTrackingPort {
+			dnsTrackingPort = v
 		} else {
 			config = append(config, corev1.EnvVar{Name: k, Value: v})
 		}
@@ -633,6 +638,7 @@ func (c *AgentController) setEnvConfig(coll *flowslatest.FlowCollector) []corev1
 
 	config = append(config, corev1.EnvVar{Name: envDedupe, Value: dedup})
 	config = append(config, corev1.EnvVar{Name: EnvDedupeJustMark, Value: dedupJustMark})
+	config = append(config, corev1.EnvVar{Name: envDNSTrackingPort, Value: dnsTrackingPort})
 	config = append(config, corev1.EnvVar{
 		Name: envAgentIP,
 		ValueFrom: &corev1.EnvVarSource{
