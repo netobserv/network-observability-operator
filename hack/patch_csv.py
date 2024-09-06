f106885bc6cbb54f5b6d3c86e541e172f656018f
Olivier Cazade <ocazade@redhat.com>
2024-09-06 14:35:42 +0000

    NETOBSERV-1792: Added update-build script to add downstream labels (#739)
    * Add script to perform downstream changes
    
    * Update hack/patch_csv.py
    
    Co-authored-by: Joel Takvorian <joel.takvorian@homeblocks.net>
    
    * Update hack/update-build.sh
    
    Co-authored-by: Joel Takvorian <joel.takvorian@homeblocks.net>
    
    ---------
    
    Co-authored-by: Joel Takvorian <joel.takvorian@homeblocks.net>

---
+++ hack/patch_csv.py
@@ -0,0 +1,83 @@
+import os
+from sys import exit as sys_exit
+from datetime import datetime
+from ruamel.yaml import YAML
+yaml = YAML()
+
+def load_manifest(pathn):
+   if not pathn.endswith(".yaml"):
+      return None
+   try:
+      with open(pathn, "r") as f:
+         return yaml.load(f)
+   except FileNotFoundError:
+      print("File can not found")
+      exit(6)
+
+def dump_manifest(pathn, manifest):
+   with open(pathn, "w") as f:
+      yaml.dump(manifest, f)
+   return
+
+timestamp = int(os.getenv('EPOC_TIMESTAMP'))
+datetime_time = datetime.fromtimestamp(timestamp)
+version = os.getenv('VERSION')
+replaces = os.getenv('REPLACES')
+desc_file_name = os.getenv('IN_CSV_DESC')
+csv = load_manifest(os.getenv('TARGET_CSV_FILE'))
+created_at = datetime_time.strftime('%Y-%m-%dT%H:%M:%S')
+operator_image = 'registry.redhat.io/network-observability/network-observability-rhel9-operator@v{}'.format(version)
+ebpf_image = 'registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@v{}'.format(version)
+flp_image ='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@v{}'.format(version)
+console_image = 'registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@v{}'.format(version)
+
+csv['metadata']['annotations']['operators.openshift.io/valid-subscription'] = '["OpenShift Kubernetes Engine", "OpenShift Container Platform", "OpenShift Platform Plus"]'
+csv['metadata']['annotations']['operatorframework.io/cluster-monitoring'] = 'true'
+csv['metadata']['name'] = 'network-observability-operator.v{}'.format(version)
+csv['metadata']['annotations']['createdAt'] = created_at
+csv['metadata']['annotations']['containerImage'] = os.getenv('OPERATOR_PULLSPEC', '')
+csv['metadata']['annotations']['features.operators.openshift.io/disconnected'] = 'true'
+csv['metadata']['annotations']['features.operators.openshift.io/fips-compliant'] = 'true'
+csv['metadata']['annotations']['features.operators.openshift.io/proxy-aware'] = 'false'
+csv['metadata']['annotations']['features.operators.openshift.io/tls-profiles'] = 'false'
+csv['metadata']['annotations']['features.operators.openshift.io/token-auth-aws'] = 'false'
+csv['metadata']['annotations']['features.operators.openshift.io/token-auth-azure'] = 'false'
+csv['metadata']['annotations']['features.operators.openshift.io/token-auth-gcp'] = 'false'
+csv['metadata']['annotations']['features.operators.openshift.io/cnf'] = 'false'
+csv['metadata']['annotations']['features.operators.openshift.io/cni'] = 'false'
+csv['metadata']['annotations']['features.operators.openshift.io/csi'] = 'false'
+
+# inject bundle creation date in pods annotations
+podMeta = csv['spec']['install']['spec']['deployments'][0]['spec']['template']['metadata']
+if 'annotations' in podMeta:
+   podMeta['annotations']['bundleCreatedAt'] = created_at
+else:
+   podMeta['annotations'] = {'bundleCreatedAt': created_at}
+
+for env in csv['spec']['install']['spec']['deployments'][0]['spec']['template']['spec']['containers'][0]['env']:
+   if env['name'] == 'DOWNSTREAM_DEPLOYMENT':
+      env['value'] = "true"
+   if env['name'] == 'RELATED_IMAGE_EBPF_AGENT':
+      env['value'] = ebpf_image
+   if env['name'] == 'RELATED_IMAGE_FLOWLOGS_PIPELINE':
+      env['value'] = flp_image
+   if env['name'] == 'RELATED_IMAGE_CONSOLE_PLUGIN':
+      env['value'] = console_image
+
+csv['spec']['install']['spec']['deployments'][0]['spec']['template']['spec']['containers'][0]['image'] = operator_image
+
+# replaces upstream description by something more OpenShift'ish
+file = open(desc_file_name,mode='r')
+csv['spec']['description'] = file.read()
+file.close()
+
+csv['spec']['displayName'] = 'Network Observability'
+csv['spec']['maturity'] = 'stable'
+
+# remove relatedImages from spec as it is picked up from ENV instead (having them in both places generates a build error)
+csv['spec'].pop('relatedImages', None)
+
+csv['spec']['version'] = version
+csv['spec']['replaces'] = 'network-observability-operator.v{}'.format(replaces)
+
+dump_manifest(os.getenv('TARGET_CSV_FILE'), csv)
---
+++ hack/update-build.sh
@@ -0,0 +1,74 @@
+
+#!/usr/bin/env bash
+
+echo "Updating container file"
+
+: "${COMMIT:=$(git rev-list --abbrev-commit --tags --max-count=1)}"
+: "${CONTAINER_FILE:=./Dockerfile}"
+: "${BUNDLE_CONTAINER_FILE:=./bundle.Dockerfile}"
+: "${CATALOG_CONTAINER_FILE:=./catalog.Dockerfile}"
+: "${TARGET_VERSION:=1.7.0}"
+: "${REPLACE_VERSION:=1.6.1}"
+
+
+supported_ocp_versions="v4.13"
+manifests_dir="./bundle/manifests"
+metadata_dir="./bundle/metadata"
+crd_name="flows.netobserv.io_flowcollectors.yaml"
+crd_file="${manifests_dir}/${crd_name}"
+csv_name="netobserv-operator.clusterserviceversion.yaml"
+csv_file="${manifests_dir}/${csv_name}"
+
+cat <<EOF >>"${CONTAINER_FILE}"
+LABEL com.redhat.component="network-observability-operator-container"
+LABEL name="network-observability-operator"
+LABEL io.k8s.display-name="Network Observability Operator"
+LABEL io.k8s.description="Network Observability Operator"
+LABEL summary="Network Observability Operator"
+LABEL maintainer="support@redhat.com"
+LABEL io.openshift.tags="network-observability-operator"
+LABEL upstream-vcs-ref="${COMMIT}"
+LABEL upstream-vcs-type="git"
+EOF
+
+echo "Updating fbc bundle file"
+
+
+cat <<EOF >>"${BUNDLE_CONTAINER_FILE}"
+LABEL com.redhat.component="network-observability-operator-bundle-container"
+LABEL name="network-observability-operator-bundle"
+LABEL io.k8s.display-name="Network Observability Operator Bundle"
+LABEL io.k8s.description="Network Observability Operator Bundle"
+LABEL summary="Network Observability Operator Bundle"
+LABEL maintainer="support@redhat.com"
+LABEL io.openshift.tags="network-observability-operator-bundle"
+LABEL upstream-vcs-ref="${COMMIT}"
+LABEL upstream-vcs-type="git"
+EOF
+
+echo "Updating catalog container file"
+
+cat <<EOF >>"${CATALOG_CONTAINER_FILE}"
+LABEL com.redhat.component="network-observability-operator-catalog-container"
+LABEL name="network-observability-operator-catalog"
+LABEL io.k8s.display-name="Network Observability Operator Catalog"
+LABEL io.k8s.description="Network Observability Operator Catalog"
+LABEL summary="Network Observability Operator Catalog"
+LABEL maintainer="support@redhat.com"
+LABEL io.openshift.tags="network-observability-operator-catalog"
+LABEL upstream-vcs-ref="${COMMIT}"
+LABEL upstream-vcs-type="git"
+EOF
+
+
+[ ! -f "${crd_file}" ] && { echo "CustomResourceDefinition file not found, the version or name might have changed on us!"; exit 5; }
+
+sed -i 's/\<NetObserv\>/network observability/g' "${crd_file}"
+
+export EPOC_TIMESTAMP=$(date +%s)
+export IN_CSV_DESC="./config/descriptions/ocp.md"
+
+REPLACES="${REPLACE_VERSION}" VERSION="${TARGET_VERSION}" TARGET_CSV_FILE="${csv_file}" python3 ./hack/patch_csv.py
+
+sed -i 's/operators.operatorframework.io.bundle.channels.v1: latest,community/operators.operatorframework.io.bundle.channels.v1: stable/g' ./bundle/metadata/annotations.yaml
+sed -i 's/operators.operatorframework.io.bundle.channel.default.v1: community/operators.operatorframework.io.bundle.channel.default.v1: stable/g' ./bundle/metadata/annotations.yaml
