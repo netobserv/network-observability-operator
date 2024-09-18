a9185e844d9a7c270f6a521e5cb5df8231d261f9
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-09-18 16:06:24 +0000

    Update network-observability-operator to 76aa5cd

--- hack/container_digest.sh
+++ hack/container_digest.sh
@@ -1,4 +1,4 @@
-export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/network-observability-operator@sha256:b7ef7cc9fd36218949638fcdc079d664629430af99485adededd0e0969d6c505'
+export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/network-observability-operator@sha256:76aa5cd9f3fbae1af3374c188457718cc0bf0ebf0ac693cc14ab1c5370518b60'
 export EBPF_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/netobserv-ebpf-agent@sha256:36a741fe7cf5d19db0622b1d7f846617612602e85a97021e9a76595d5e4ffdc1'
 export FLP_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/flowlogs-pipeline@sha256:4049ec904072ac7d8c20e98a14ab2d3e89dc3b4b5abc5f5dffd1c0b85267b443'
 export CONSOLE_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/network-observability-console-plugin@sha256:5f2352a2d0fba28f4a00578892bc7993e6a363959f1e4956d7ab74ab0fccb829'
