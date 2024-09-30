8979fdfac3ab8d1bbd10be20ade794c2e533e480
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-09-30 14:58:45 +0000

    Update network-observability-console-plugin to d31de30
    Signed-off-by: red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>

--- hack/container_digest.sh
+++ hack/container_digest.sh
@@ -1,4 +1,4 @@
 export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/network-observability-operator@sha256:76aa5cd9f3fbae1af3374c188457718cc0bf0ebf0ac693cc14ab1c5370518b60'
 export EBPF_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/netobserv-ebpf-agent@sha256:eb43914d8285d0fcf9eaa52c08eb86f8a4b9f1afb6eadb32f7abe246d951431d'
 export FLP_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/flowlogs-pipeline@sha256:76e6026c306450554b4f7ad710470aad7a13942c5a07bac7fdf36b1158fd0e61'
-export CONSOLE_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/network-observability-console-plugin@sha256:5f2352a2d0fba28f4a00578892bc7993e6a363959f1e4956d7ab74ab0fccb829'
+export CONSOLE_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/network-observability-console-plugin@sha256:d31de305a2d73b5fd2b1f26b88bc8644345c07abb6dad19d2c4b427b2f8e3e5f'
