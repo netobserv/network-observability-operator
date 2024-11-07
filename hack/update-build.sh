ace52740f9922b1c1520c8242e6cc04ec5f4c352
Olivier Cazade <ocazade@redhat.com>
2024-11-07 09:21:30 +0000

    Enable konflux multi arch build (#830)

--- hack/update-build.sh
+++ hack/update-build.sh
@@ -7,8 +7,8 @@ echo "Updating container file"
 : "${CONTAINER_FILE:=./Dockerfile}"
 : "${BUNDLE_CONTAINER_FILE:=./bundle.Dockerfile}"
 : "${CATALOG_CONTAINER_FILE:=./catalog.Dockerfile}"
-: "${TARGET_VERSION:=1.7.0}"
-: "${REPLACE_VERSION:=1.6.1}"
+: "${TARGET_VERSION:=1.7.1}"
+: "${REPLACE_VERSION:=1.7.0}"
 
 
 supported_ocp_versions="v4.13"
@@ -22,51 +22,6 @@ index_file="./catalog/index.yaml"
 
 source ./hack/container_digest.sh
 source ./hack/bundle_digest.sh
-
-cat <<EOF >>"${CONTAINER_FILE}"
-LABEL com.redhat.component="network-observability-operator-container"
-LABEL name="network-observability-operator"
-LABEL io.k8s.display-name="Network Observability Operator"
-LABEL io.k8s.description="Network Observability Operator"
-LABEL summary="Network Observability Operator"
-LABEL maintainer="support@redhat.com"
-LABEL io.openshift.tags="network-observability-operator"
-LABEL upstream-vcs-ref="${COMMIT}"
-LABEL upstream-vcs-type="git"
-LABEL description="NetObserv Operator is a Kubernetes / OpenShift operator for network observability."
-EOF
-
-echo "Updating fbc bundle file"
-
-
-cat <<EOF >>"${BUNDLE_CONTAINER_FILE}"
-LABEL com.redhat.component="network-observability-operator-bundle-container"
-LABEL name="network-observability-operator-bundle"
-LABEL io.k8s.display-name="Network Observability Operator Bundle"
-LABEL io.k8s.description="Network Observability Operator Bundle"
-LABEL summary="Network Observability Operator Bundle"
-LABEL maintainer="support@redhat.com"
-LABEL io.openshift.tags="network-observability-operator-bundle"
-LABEL upstream-vcs-ref="${COMMIT}"
-LABEL upstream-vcs-type="git"
-LABEL description="NetObserv Operator is a Kubernetes / OpenShift operator for network observability."
-EOF
-
-echo "Updating catalog container file"
-
-cat <<EOF >>"${CATALOG_CONTAINER_FILE}"
-LABEL com.redhat.component="network-observability-operator-catalog-container"
-LABEL name="network-observability-operator-catalog"
-LABEL io.k8s.display-name="Network Observability Operator Catalog"
-LABEL io.k8s.description="Network Observability Operator Catalog"
-LABEL summary="Network Observability Operator Catalog"
-LABEL maintainer="support@redhat.com"
-LABEL io.openshift.tags="network-observability-operator-catalog"
-LABEL upstream-vcs-ref="${COMMIT}"
-LABEL upstream-vcs-type="git"
-LABEL description="NetObserv Operator is a Kubernetes / OpenShift operator for network observability."
-EOF
-
 
 [ ! -f "${crd_file}" ] && { echo "CustomResourceDefinition file not found, the version or name might have changed on us!"; exit 5; }
 
@@ -82,4 +37,4 @@ sed -i 's/operators.operatorframework.io.bundle.channels.v1: latest,community/op
 sed -i 's/operators.operatorframework.io.bundle.channel.default.v1: community/operators.operatorframework.io.bundle.channel.default.v1: stable/g' ./bundle/metadata/annotations.yaml
 
 #Using downstream base image
-sed -i 's/\(FROM.*\)docker.io\/library\/golang:1.22\(.*\)/\1brew.registry.redhat.io\/rh-osbs\/openshift-golang-builder:v1.22.5-202407301806.g4c8b32d.el9\2/g' ./Dockerfile
+echo "Container file updated"
