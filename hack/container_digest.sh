bb01eaa20b1b85f43ae6fd0f5fe49f8ad755b31c
Olivier Cazade <ocazade@redhat.com>
2024-11-11 22:46:50 +0000

    Update image reference to point to final repository

--- hack/container_digest.sh
+++ hack/container_digest.sh
@@ -1,4 +1,4 @@
-export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/network-observability-operator@sha256:76aa5cd9f3fbae1af3374c188457718cc0bf0ebf0ac693cc14ab1c5370518b60'
-export EBPF_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/netobserv-ebpf-agent@sha256:36a741fe7cf5d19db0622b1d7f846617612602e85a97021e9a76595d5e4ffdc1'
-export FLP_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/flowlogs-pipeline@sha256:76e6026c306450554b4f7ad710470aad7a13942c5a07bac7fdf36b1158fd0e61'
-export CONSOLE_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/ocp-network-observab-tenant/netobserv-operator/network-observability-console-plugin@sha256:5f2352a2d0fba28f4a00578892bc7993e6a363959f1e4956d7ab74ab0fccb829'
+export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:76aa5cd9f3fbae1af3374c188457718cc0bf0ebf0ac693cc14ab1c5370518b60'
+export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:36a741fe7cf5d19db0622b1d7f846617612602e85a97021e9a76595d5e4ffdc1'
+export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:76e6026c306450554b4f7ad710470aad7a13942c5a07bac7fdf36b1158fd0e61'
+export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:5f2352a2d0fba28f4a00578892bc7993e6a363959f1e4956d7ab74ab0fccb829'
