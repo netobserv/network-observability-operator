c77bb68d7702b41b61594876295caf8407856417
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-11-12 01:21:52 +0000

    chore(deps): update network-observability-operator-1-7 to 0efa4a2
    Signed-off-by: red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>

--- hack/container_digest.sh
+++ hack/container_digest.sh
@@ -1,4 +1,4 @@
-export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:76aa5cd9f3fbae1af3374c188457718cc0bf0ebf0ac693cc14ab1c5370518b60'
+export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:0efa4a25bc1dd720aba2e47cab37e50177e38bd2ea5ad559975e4c4daec6e308'
 export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:36a741fe7cf5d19db0622b1d7f846617612602e85a97021e9a76595d5e4ffdc1'
 export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:76e6026c306450554b4f7ad710470aad7a13942c5a07bac7fdf36b1158fd0e61'
 export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:5f2352a2d0fba28f4a00578892bc7993e6a363959f1e4956d7ab74ab0fccb829'
