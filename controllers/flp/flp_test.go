00b45f18b44349d0d43df74cf3bd500c7ec530e7
Julien Pinsonneau <jpinsonn@redhat.com>
2024-11-27 10:55:36 +0100

    fix tests

--- controllers/flp/flp_test.go
+++ controllers/flp/flp_test.go
@@ -657,6 +657,7 @@ func TestConfigMapShouldDeserializeAsJSONWithLokiManual(t *testing.T) {
 		"DstK8S_Type",
 		"K8S_FlowLayer",
 		"FlowDirection",
+		"UdnId",
 		"_RecordType",
 	}, lokiCfg.Labels)
 	assert.Equal(`{app="netobserv-flowcollector"}`, fmt.Sprintf("%v", lokiCfg.StaticLabels))
@@ -713,6 +714,7 @@ func TestConfigMapShouldDeserializeAsJSONWithLokiStack(t *testing.T) {
 		"DstK8S_Type",
 		"K8S_FlowLayer",
 		"FlowDirection",
+		"UdnId",
 		"_RecordType",
 	}, lokiCfg.Labels)
 	assert.Equal(`{app="netobserv-flowcollector"}`, fmt.Sprintf("%v", lokiCfg.StaticLabels))
