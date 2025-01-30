37d1027d307ce9eb8fcdb934f4c39aa1da859373
Olivier Cazade <ocazade@redhat.com>
2025-01-30 14:51:26 +0100

    1.8 digest update (#1078)
    * Fixe typo in bundle images digests (#1075)
    
    * Updating images digests

--- hack/container_digest.sh
+++ hack/container_digest.sh
@@ -1,8 +1,9 @@
 # Do not remove empty lines, they are there to reduce conflicts
-export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:ac188a5e24208a982f8533c7489d9b7485e1999aa984c4d3ca3547aab66cad59'
+export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:3ec946b4d17b8898d0edfc6878aab54aa6357926344f8b3836fbbc3edd2570d6'
 #
-export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:d6264a772128eeaae71e7845eec86da28f98dcbf3a9f715599c16c66c1a6f4b8'
+export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:b413fdb6c65802ba6c41ec5e4aaa9bb81b08df52d791d3dae219b2780be10854'
 #
-export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:e3b3006b1b28b1b6a515d0908def56636d48d1a43bd7f3fcd7e52a0f10817c23'
+export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:34d3add83cafc362eac6b9eb000e96dc873c5ea38ac92ae9bfd9c3ff15cf744d'
 #
-export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:17c58daa0a281cf93f68d948b0723491ffeb1a7f1a9218cd2a3e9a89d97678a6'
+export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:043e68abe535fc0d900bfbf9affcf2e295a564431767493cd5a87434e0f7d049'
+
