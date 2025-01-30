8c4fb73db156f2bcb45fe11957e9aabb340f84b6
Olivier Cazade <ocazade@redhat.com>
2025-01-30 21:50:06 +0100

    Bumping images digests (#1083)

--- hack/container_digest.sh
+++ hack/container_digest.sh
@@ -1,9 +1,8 @@
 # Do not remove empty lines, they are there to reduce conflicts
-export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:3ec946b4d17b8898d0edfc6878aab54aa6357926344f8b3836fbbc3edd2570d6'
+export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:25acc54e5b7db3911dd7986df06d65953a6268e3dbae3365b799a78d50fcc464'
 #
-export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:b413fdb6c65802ba6c41ec5e4aaa9bb81b08df52d791d3dae219b2780be10854'
+export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:716008ee8ba409476b3a62c3ff03d4d31c03fa985c134969ac195acddb2def70'
 #
-export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:34d3add83cafc362eac6b9eb000e96dc873c5ea38ac92ae9bfd9c3ff15cf744d'
+export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:e48244e9413830cbc35ecdec22a009de0c049a06c1cace7bcd17bc78d2ae7592'
 #
 export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:043e68abe535fc0d900bfbf9affcf2e295a564431767493cd5a87434e0f7d049'
-
