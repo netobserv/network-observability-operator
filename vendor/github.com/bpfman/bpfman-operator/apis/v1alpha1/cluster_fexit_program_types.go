31d341c9c831fd4808a1f917114dcd911d590174
Mohamed Mahmoud <mmahmoud@redhat.com>
2025-03-06 09:52:55 -0500

    migrate to recent ebpf manager APIs
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

---
+++ vendor/github.com/bpfman/bpfman-operator/apis/v1alpha1/cluster_fexit_program_types.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2024 The bpfman Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// All fields are required unless explicitly marked optional
+package v1alpha1
+
+// ClFexitProgramInfo defines the Fexit program details
+type ClFexitProgramInfo struct {
+	ClFexitLoadInfo `json:",inline"`
+	// Whether the program should be attached to the function.
+	// +optional
+	// +kubebuilder:validation:MaxItems=1
+	Links []ClFexitAttachInfo `json:"links,omitempty"`
+}
+
+// ClFexitLoadInfo contains the program-specific load information for Fexit
+// programs
+type ClFexitLoadInfo struct {
+	// function is the name of the function to attach the Fexit program to.
+	// +kubebuilder:validation:Pattern="^[a-zA-Z][a-zA-Z0-9_]+."
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=64
+	Function string `json:"function"`
+}
+
+// ClFexitAttachInfo indicates that the Fentry program should be attached to
+// the function identified in ClFentryLoadInfo. The only valid value for Attach
+// is true.
+type ClFexitAttachInfo struct {
+	// +kubebuilder:validation:Enum=Attach;Dettach;
+	Mode AttachTypeAttach `json:"mode"`
+}
+
+type ClFexitProgramInfoState struct {
+	ClFexitLoadInfo `json:",inline"`
+	// +optional
+	// +kubebuilder:validation:MaxItems=1
+	Links []ClFexitAttachInfoState `json:"links,omitempty"`
+}
+
+type ClFexitAttachInfoState struct {
+	AttachInfoStateCommon `json:",inline"`
+}
---
+++ vendor/github.com/bpfman/bpfman-operator/apis/v1alpha1/cluster_kretprobe_program_types.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2023 The bpfman Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// All fields are required unless explicitly marked optional
+package v1alpha1
+
+// ClKprobeProgramInfo contains the information for the kprobe program
+type ClKretprobeProgramInfo struct {
+	// The list of points to which the program should be attached.  The list items
+	// are optional and may be udated after the bpf program has been loaded
+	// +optional
+	Links []ClKretprobeAttachInfo `json:"links,omitempty"`
+}
+
+type ClKretprobeAttachInfo struct {
+	// function to attach the kprobe to.
+	// +kubebuilder:validation:Pattern="^[a-zA-Z][a-zA-Z0-9_]+."
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=64
+	Function string `json:"function"`
+}
+
+type ClKretprobeProgramInfoState struct {
+	// List of attach points for the BPF program on the given node. Each entry
+	// in *AttachInfoState represents a specific, unique attach point that is
+	// derived from *AttachInfo by fully expanding any selectors.  Each entry
+	// also contains information about the attach point required by the
+	// reconciler
+	// +optional
+	Links []ClKretprobeAttachInfoState `json:"links,omitempty"`
+}
+
+type ClKretprobeAttachInfoState struct {
+	AttachInfoStateCommon `json:",inline"`
+
+	// Function to attach the kprobe to.
+	Function string `json:"function"`
+}
