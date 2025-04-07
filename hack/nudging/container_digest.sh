0223f68e3814bac99099129a53966fec7c5e584c
Joel Takvorian <jtakvori@redhat.com>
2025-04-07 14:18:10 +0200

    manual nudging

--- hack/nudging/container_digest.sh
+++ hack/nudging/container_digest.sh
@@ -1,8 +1,8 @@
 # Do not remove empty lines, they are there to reduce conflicts
-export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:1958de7a072c987a44410bac9d23c68c80e7a7f9dc60e3d812054edb292d5feb'
+export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:5197e89bd62ddd274df0e0edf360c5fb170750811572fce95ebdf8f09cedf287'
 #
-export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:6575396cf2874188f362378b6d4e9bfb9a7253d79aa7c9e47c8e8ca3de3ed76c'
+export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:2eebe07e954f966b35c381002b8c4c83f7f12d844be1110a7bf22985afa4e9af'
 #
-export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:b93223495882690f6ff4156cf74114139393ec5a5d6392864b6c735aee790ead'
+export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:1fd946cf755147895fcb1b7e0b3e35eaddb313ca5fce36d32a274d3e4e7d0896'
 #
-export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:6b48610e520879f658de60b51e34d648ac124b30b61351eba98e8f92323015a6'
+export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:ee5a78a26ffe486dd7a20d9cad8955b8ef1e50a4700a7df6d2cf1e9227ea517e'
