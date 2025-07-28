bd77e7078b9649af2b9c4784e710bf3fd1f96c8a
Joel Takvorian <jtakvori@redhat.com>
2025-07-28 11:06:44 +0200

    Squashed commit of: 1793,1794

--- hack/nudging/container_digest.sh
+++ hack/nudging/container_digest.sh
@@ -4,8 +4,8 @@ export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network
 # eBPF agent
 export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:72b01adc65269ddbf5db732d550bc2ae7acc248561c81166c2989bcbf079df62'
 # Flowlogs-pipeline
-export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:6ee3e553233272d7d425ec32998488e275069326cee5c8ecf1e0131359858ae3'
+export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:c49e5dbcafa10e45dc0443fce285020ee0e2c496dccb908a29f846995c3ccb49'
 # Console plugin
 export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:b6988ba407ed4de47386e2aa6a6f28e990e9ee3bf1253660130428ef1a64b26a'
 # Compatibility Console plugin
-export CONSOLE_COMPAT_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-compat-rhel9@sha256:083ec920370517417ffff44adcbad61f75adb79fc3e60867e2eea27b598196bd'
+export CONSOLE_COMPAT_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-compat-rhel9@sha256:f38714dd4a1ad45a0c83965f4da15b776523ae6707a2833abdbeee5de60cf25c'
