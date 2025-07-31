de1eb6b80d65cec65fdcfe13bb1ed6a01d3bdaee
Joel Takvorian <jtakvori@redhat.com>
2025-07-31 13:31:00 +0200

    Squashed commit of: 1825,1823,1821,1820

--- hack/nudging/container_digest.sh
+++ hack/nudging/container_digest.sh
@@ -2,10 +2,10 @@
 # Operator
 export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:7383bf4b997bc7d6bf82228b34340d51c58ee1a8b364c4f6f09035113e03553e'
 # eBPF agent
-export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:d13fdc6f42ce34131ddbafb21104a351f1d9160ca2018907d9370be1afd9304a'
+export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:f32ebe43c5ab2f9cad1439c1d943abc5c0bf6569709c9a22e79fe55f1352f053'
 # Flowlogs-pipeline
-export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:c49e5dbcafa10e45dc0443fce285020ee0e2c496dccb908a29f846995c3ccb49'
+export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:a9dff28964cd16ce59ba5190e5ab71aa1ca8bbc7ca007b1df66a5b7af850a102'
 # Console plugin
-export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:b6988ba407ed4de47386e2aa6a6f28e990e9ee3bf1253660130428ef1a64b26a'
+export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:c8073f9efe6bd3ec6535592c6bd9c5cee647f084c0cbb6510b06d2bcd6d1dc51'
 # Compatibility Console plugin
-export CONSOLE_COMPAT_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-compat-rhel9@sha256:f38714dd4a1ad45a0c83965f4da15b776523ae6707a2833abdbeee5de60cf25c'
+export CONSOLE_COMPAT_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-compat-rhel9@sha256:06f2ffd0ccfd483dd136940285f1999b5054dda33cc32ae1192bbfe6033b8f19'
