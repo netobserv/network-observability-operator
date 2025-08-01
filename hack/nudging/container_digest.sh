8674810471fb833baeb1d0c7103b3dc41bd322a5
Joel Takvorian <jtakvori@redhat.com>
2025-08-01 16:37:44 +0200

    Squashed commit of: 1828,1829,1831

--- hack/nudging/container_digest.sh
+++ hack/nudging/container_digest.sh
@@ -1,10 +1,10 @@
 # Do not remove comment lines, they are there to reduce conflicts
 # Operator
-export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:7383bf4b997bc7d6bf82228b34340d51c58ee1a8b364c4f6f09035113e03553e'
+export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:8bb947f5880b4d7b30cc8d8be65ad8b60d8c816fe0be2334de81ebf4dfdceffc'
 # eBPF agent
-export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:f32ebe43c5ab2f9cad1439c1d943abc5c0bf6569709c9a22e79fe55f1352f053'
+export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:5acaa9a2d1a45ad3ae681e1c71f3a57ddeeb4b6ef2b9a17d0c487deeb7275737'
 # Flowlogs-pipeline
-export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:a9dff28964cd16ce59ba5190e5ab71aa1ca8bbc7ca007b1df66a5b7af850a102'
+export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:15a8199a739c4dec58eb3ce05c7c707fb58c6e0abfbddf5889a228be4bb8a1c9'
 # Console plugin
 export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:c8073f9efe6bd3ec6535592c6bd9c5cee647f084c0cbb6510b06d2bcd6d1dc51'
 # Compatibility Console plugin
