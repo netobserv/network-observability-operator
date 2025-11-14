a44e234d6e04656671a6affb83c72667dd7c8a70
Luis Jira <ljira@redhat.com>
2025-11-14 17:42:06 +0100

    Refresh vendors

---
+++ vendor/github.com/netobserv/netobserv-ebpf-agent/pkg/maps/maps.go
@@ -0,0 +1,15 @@
+package maps
+
+// Map names
+var Maps = []string{
+	"direct_flows",
+	"aggregated_flows",
+	"additional_flow_metrics",
+	"packet_record",
+	"dns_flows",
+	"global_counters",
+	"filter_map",
+	"peer_filter_map",
+	"ipsec_ingress_map",
+	"ipsec_egress_map",
+}
