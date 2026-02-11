9275f4647065f51d0566997a1dcc1027a10baef5
Joel Takvorian <jtakvori@redhat.com>
2026-02-11 16:25:19 +0100

    Set GOMEMLIMIT on FLP
    (Same as done on the agent)

--- internal/pkg/helper/env.go
+++ internal/pkg/helper/env.go
@@ -1,6 +1,8 @@
 package helper
 
 import (
+	"fmt"
+
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -23,4 +25,17 @@ func BuildEnvFromDefaults(config, defaults map[string]string) []corev1.EnvVar {
 		}
 	}
 	return result
+}
+
+func EnvFromReqsLimits(envs []corev1.EnvVar, reqs *corev1.ResourceRequirements) []corev1.EnvVar {
+	// set GOMEMLIMIT which allows specifying a soft memory cap to force GC when resource limit is reached to prevent OOM
+	if reqs.Limits.Memory() != nil {
+		if memLimit, ok := reqs.Limits.Memory().AsInt64(); ok {
+			// we will set the GOMEMLIMIT to current memlimit - 10% as a headroom to account for
+			// memory sources the Go runtime is unaware of
+			memLimit -= int64(float64(memLimit) * 0.1)
+			envs = append(envs, corev1.EnvVar{Name: "GOMEMLIMIT", Value: fmt.Sprint(memLimit)})
+		}
+	}
+	return envs
 }
