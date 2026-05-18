3d7e53429c111281752171b66624fd6efcbc79bd
Joel Takvorian <jtakvori@redhat.com>
2026-05-18 13:52:26 +0200

    Squashed commit of: 2749,2750,2751,2753,2757

--- hack/nudging/container_digest.sh
+++ hack/nudging/container_digest.sh
@@ -1,11 +1,11 @@
 # Do not remove comment lines, they are there to reduce conflicts
 # Operator
-export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:d47a55e9eb7c1781dbb0d8edbf398eab38b01028c72ed9174ceba67ea7243f4a'
+export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-rhel9-operator@sha256:69b6d3915a960d3de3fa508baaea31643b3f0e22e4529073ccf0324341a02f57'
 # eBPF agent
-export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:a34eadad1fb6a700e6ced20700f3ffc11b9235ebc241b3868b2faa5c684b3cfe'
+export EBPF_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-ebpf-agent-rhel9@sha256:e108a8cf2d1e71c6f6d692c1ba768cc2c70da66282a6533fb4eee66d0de22878'
 # Flowlogs-pipeline
-export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:3178bbac5e57544db841afcb4922299ff1d343b96a2b2f214dd0ab0407220378'
+export FLP_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-flowlogs-pipeline-rhel9@sha256:9e8488ed9234e339a4375a0be132dd4f7763df01889ad46519b21a9385caffd0'
 # Console plugin
-export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:0251096b7990ff5464a794e72a18b91c6f41ba4c365949d314c50c9f124356d9'
+export CONSOLE_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-rhel9@sha256:e5a57c9c2857de0d6f52c4f382f54854e37dc5c77e56f1e536a7f5c8ba94cac9'
 # Compatibility Console plugin
-export CONSOLE_COMPAT_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-compat-rhel9@sha256:64fb2b76bebde1a561fe60f574f6188609fe4ba479ceaa49ef3c556babc24c6b'
+export CONSOLE_COMPAT_IMAGE_PULLSPEC='registry.redhat.io/network-observability/network-observability-console-plugin-compat-rhel9@sha256:87fb1d2799d2f150533d0c1a50b6be6167cc3ece09ce31179b2dfc5576ad0f9c'
