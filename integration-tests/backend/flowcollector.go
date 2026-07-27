4431ae4adf8d49cb91275eb5b34a0f6dbd224367
memodi <memodi@redhat.com>
2026-07-27 18:24:39 -0400

    add a check for informers deployment to be ready

--- integration-tests/backend/flowcollector.go
+++ integration-tests/backend/flowcollector.go
@@ -212,6 +212,9 @@ func (flow *Flowcollector) WaitForFlowcollectorReady(oc *exutil.CLI) {
 	default:
 		waitUntilDaemonSetReady(oc, "flowlogs-pipeline", flow.Namespace)
 	}
+	// check informers deployment
+	waitUntilDeploymentReady(oc, "flowlogs-pipeline-informers", flow.Namespace)
+
 	// check ebpf-agent status
 	waitUntilDaemonSetReady(oc, "netobserv-ebpf-agent", flow.Namespace+"-privileged")
 
